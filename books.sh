373bebb000e1f341d4ae6da3cdcac2369a2f9f31
codingepaduli <padulodeveloper@protonmail.com>
2022-08-26 16:20:26 +0200

    updating script for generating more than a single books and moving the book's chapters into the books.sh file

---
+++ books.sh
@@ -0,0 +1,57 @@
+: ' commento multilinea
+
+Elenco di ebook da creare a partire dagli articoli elencati
+
+'
+
+BOOKNAME_JAVASCRIPT="Appunti-di-laboratorio-di-informatica-su-javascript"
+CHAPTERS_JAVASCRIPT="
+        $CONTENT_DIR/content/coding/web/javascript/intro.md
+        $CONTENT_DIR/content/coding/web/javascript/primi_script.md
+        $CONTENT_DIR/content/coding/web/javascript/Variabili.md
+        $CONTENT_DIR/content/coding/web/javascript/conversioni.md
+        $CONTENT_DIR/content/coding/web/javascript/istruzioni_condizionali_iterative.md
+        $CONTENT_DIR/content/coding/web/javascript/funzioni.md
+        $CONTENT_DIR/content/coding/web/javascript/interazioneHtml.md
+        $CONTENT_DIR/content/coding/web/javascript/selettoriCSS.md
+        $CONTENT_DIR/content/coding/web/javascript/DOM.md
+        "
+
+: ' da verificare
+        $CONTENT_DIR/content/coding/web/javascript/operatori.md
+'
+
+BOOKNAME_HTML="Appunti-di-laboratorio-di-informatica-su-html5"
+CHAPTERS_HTML="
+        $CONTENT_DIR/content/coding/web/html/intro.md
+        $CONTENT_DIR/content/coding/web/html/etichette.md
+        $CONTENT_DIR/content/coding/web/html/tagTesto.md
+        $CONTENT_DIR/content/coding/web/html/tagAttributes.md
+        $CONTENT_DIR/content/coding/web/html/metadati.md
+        $CONTENT_DIR/content/coding/web/html/validazione.md
+        $CONTENT_DIR/content/coding/web/html/codificaUnicode.md
+        $CONTENT_DIR/content/coding/web/html/Accessibilita.md
+        "
+
+BOOKNAME_SISTEMI="Appunti-di-laboratorio-sistemi-e-reti"
+CHAPTERS_SISTEMI="
+        $CONTENT_DIR/content/coding/tools/EditorConfig.md
+        $CONTENT_DIR/content/coding/tools/Git.md
+        $CONTENT_DIR/content/coding/tools/MacchineVirtuali.md
+        $CONTENT_DIR/content/coding/tools/VirtualBox.md
+        $CONTENT_DIR/content/coding/tools/AQemu.md
+        $CONTENT_DIR/content/coding/tools/Vagrant.md
+        $CONTENT_DIR/content/coding/shell/PowerShell/intro.md
+        $CONTENT_DIR/content/coding/shell/PowerShell/Concetti.md
+        $CONTENT_DIR/content/coding/shell/PowerShell/PrimiScript.md
+        $CONTENT_DIR/content/coding/shell/PowerShell/CmdLet-Filesystem-Path.md
+        $CONTENT_DIR/content/coding/shell/PowerShell/CmdLet-Filesystem-FileCartelle.md
+        "
+
+BOOKNAME_P5_JS="Appunti-di-laboratorio-di-informatica-su-p5-js"
+CHAPTERS_P5_JS="
+        $CONTENT_DIR/content/coding/web/p5js/intro.md
+        $CONTENT_DIR/content/coding/web/p5js/graphicsPrimitives.md
+        $CONTENT_DIR/content/coding/web/p5js/colorsAndStyles.md
+        $CONTENT_DIR/content/coding/web/p5js/interactivity.md
+        "
--- generate.sh
+++ generate.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "Current dir: $CURRENT_DIR"
+
 BUILD="$CURRENT_DIR/build/"
 CONTENT_DIR="$CURRENT_DIR/../codingepaduli" # NO ending slash "/"
 RESOURCE_DIR="$CURRENT_DIR/../codingepaduli/static"
+echo "Build dir: $BUILD"
+echo "Content dir: $CONTENT_DIR"
+echo "Resource dir: $RESOURCE_DIR"
+
+source books.sh
+
+declare -A books
+books[0,0]=$BOOKNAME_HTML
+books[0,1]=$CHAPTERS_HTML
+books[1,0]=$BOOKNAME_JAVASCRIPT
+books[1,1]=$CHAPTERS_JAVASCRIPT
+books[2,0]=$BOOKNAME_P5_JS
+books[2,1]=$CHAPTERS_P5_JS
 
 METADATA="metadata.xml"
 
@@ -12,75 +27,6 @@ FIRST_PAGE="$CURRENT_DIR/cover.md"
 
 IMAGE_PREPROCESS_FILTER_EBOOK="replace_image_source.lua"
 PAGEBREAK_PREPROCESS_FILTER="pagebreak.lua"
-
-: ' commento multilinea
-BOOKNAME="Appunti-di-laboratorio-Sistemi-Reti"
-CHAPTERS="
-          $CONTENT_DIR/content/coding/tools/EditorConfig.md
-          $CONTENT_DIR/content/coding/tools/Git.md
-          $CONTENT_DIR/content/coding/tools/MacchineVirtuali.md
-          $CONTENT_DIR/content/coding/tools/VirtualBox.md
-          $CONTENT_DIR/content/coding/tools/AQemu.md
-          $CONTENT_DIR/content/coding/tools/Vagrant.md
-          $CONTENT_DIR/content/coding/shell/PowerShell/intro.md
-          $CONTENT_DIR/content/coding/shell/PowerShell/Concetti.md
-          $CONTENT_DIR/content/coding/shell/PowerShell/PrimiScript.md
-          $CONTENT_DIR/content/coding/shell/PowerShell/CmdLet-Filesystem-Path.md
-          $CONTENT_DIR/content/coding/shell/PowerShell/CmdLet-Filesystem-FileCartelle.md"
-'
-
-: ' commento multilinea
-BOOKNAME="Appunti-di-laboratorio-TPSIT"
-CHAPTERS="
-          $CONTENT_DIR/content/coding/web/html/intro.md
-          $CONTENT_DIR/content/coding/web/html/etichette.md
-          $CONTENT_DIR/content/coding/web/html/tagTesto.md
-          $CONTENT_DIR/content/coding/web/html/tagAttributes.md
-          $CONTENT_DIR/content/coding/web/html/metadati.md
-          $CONTENT_DIR/content/coding/web/html/codificaUnicode.md
-          $CONTENT_DIR/content/coding/web/html/validazione.md
-          $CONTENT_DIR/content/coding/web/html/immagini.md
-          $CONTENT_DIR/content/coding/web/javascript/intro.md
-          $CONTENT_DIR/content/coding/web/javascript/primi_script.md
-          $CONTENT_DIR/content/coding/web/javascript/Variabili.md
-          $CONTENT_DIR/content/coding/web/javascript/operatori.md
-          $CONTENT_DIR/content/coding/web/javascript/istruzioni_condizionali_iterative.md
-          $CONTENT_DIR/content/coding/web/javascript/funzioni.md
-          $CONTENT_DIR/content/coding/web/javascript/interazioneHtml.md
-          $CONTENT_DIR/content/coding/web/javascript/selettoriCSS.md
-          $CONTENT_DIR/content/coding/tools/Gradle.md"
-'
-
-: ' commento multilinea
-BOOKNAME="Appunti-di-laboratorio-di-informatica-per-telecomunicazioni"
-CHAPTERS="
-          $CONTENT_DIR/content/coding/web/html/intro.md
-          $CONTENT_DIR/content/coding/web/html/etichette.md
-          $CONTENT_DIR/content/coding/web/html/tagTesto.md
-          $CONTENT_DIR/content/coding/web/html/tagAttributes.md
-          $CONTENT_DIR/content/coding/web/html/metadati.md
-          $CONTENT_DIR/content/coding/web/html/codificaUnicode.md
-          $CONTENT_DIR/content/coding/web/html/validazione.md
-          "
-          #$CONTENT_DIR/content/coding/web/html/immagini.md
-'
-
-BOOKNAME="Appunti-di-laboratorio-di-informatica-su-p5-js"
-CHAPTERS="
-        $CONTENT_DIR/content/coding/web/javascript/intro.md
-        $CONTENT_DIR/content/coding/web/javascript/primi_script.md
-        $CONTENT_DIR/content/coding/web/javascript/Variabili.md
-        $CONTENT_DIR/content/coding/web/p5js/intro.md
-        $CONTENT_DIR/content/coding/web/p5js/colorsAndStyles.md
-        $CONTENT_DIR/content/coding/web/p5js/interactivity.md
-        "
-: ' commento multilinea
-        $CONTENT_DIR/content/coding/web/javascript/intro.md
-        $CONTENT_DIR/content/coding/web/javascript/primi_script.md
-        $CONTENT_DIR/content/coding/web/javascript/Variabili.md
-        $CONTENT_DIR/content/coding/web/javascript/primi_script_exe.md
-        $CONTENT_DIR/content/coding/web/javascript/operatori.md
-'
 
 # Common pandoc command for all formats
 PANDOC_COMMAND="pandoc --standalone --from=markdown+yaml_metadata_block --toc --toc-depth=3 --lua-filter=$CURRENT_DIR/$IMAGE_PREPROCESS_FILTER_EBOOK --lua-filter=$CURRENT_DIR/$PAGEBREAK_PREPROCESS_FILTER --resource-path=$RESOURCE_DIR "  # --fail-if-warnings --top-level-division=section
@@ -98,26 +44,23 @@ mkdir -p $BUILD
 
 cd $CONTENT_DIR
 
-echo "Generating ebook"
-PANDOC_COMMAND_EBOOK="$PANDOC_COMMAND --output=$BUILD$BOOKNAME.epub $CURRENT_DIR/ebook_title.txt $CHAPTERS --epub-chapter-level=1 --epub-metadata=$CURRENT_DIR/epub_metadata.xml --epub-cover-image=$CURRENT_DIR/cover_ebook.jpg --css=$CURRENT_DIR/$STYLESHEET --listings" #
+for ((i=0; i<3; i++))
+do
 
-$PANDOC_COMMAND_EBOOK
+    echo -ne "Generating pdf ${books[${i},0]} \n"
+    PANDOC_COMMAND_PDF="  $PANDOC_COMMAND --output=$BUILD${books[${i},0]}.pdf $FIRST_PAGE $CURRENT_DIR/ebook_title.txt  ${books[${i},1]}     --to=latex --pdf-engine=xelatex --top-level-division=chapter --number-sections -V geometry:margin=2cm --highlight-style=tango --css=$CURRENT_DIR/$STYLESHEET" # --verbose --metadata-file=metadata.yml -V documentclass=scrreprt
 
-echo "Generating pdf"
-PANDOC_COMMAND_PDF="  $PANDOC_COMMAND --output=$BUILD$BOOKNAME.pdf $FIRST_PAGE $CURRENT_DIR/ebook_title.txt  $CHAPTERS     --to=latex --pdf-engine=xelatex --top-level-division=chapter --number-sections -V geometry:margin=2cm --highlight-style=tango --css=$CURRENT_DIR/$STYLESHEET" # --verbose --metadata-file=metadata.yml -V documentclass=scrreprt
+    $PANDOC_COMMAND_PDF
 
-$PANDOC_COMMAND_PDF
+    echo -ne "Generating ebook ${books[${i},0]} \n"
+    PANDOC_COMMAND_EBOOK="$PANDOC_COMMAND --output=$BUILD${books[${i},0]}.epub $CURRENT_DIR/ebook_title.txt ${books[${i},1]} --epub-chapter-level=1 --epub-metadata=$CURRENT_DIR/epub_metadata.xml --epub-cover-image=$CURRENT_DIR/cover_ebook.jpg --css=$CURRENT_DIR/$STYLESHEET --listings" #
 
+    $PANDOC_COMMAND_EBOOK
 
-#@REM OPTION 1 for PDF: Use HTML5 rendering engine (wkhtmltopdf)
-#@REM pandoc -o %BUILD%%BOOKNAME%.pdf %CURRENT_DIR%\%TITLE% %CHAPTERS% %TOC% -t html5 --standalone
+    # Generating open document
+    echo -ne "Generating open document ${books[${i},0]} \n"
+    PANDOC_COMMAND_ODT="$PANDOC_COMMAND --output=$BUILD${books[${i},0]}.odt ${books[${i},1]}  --to=odt"
 
-#@REM OPTION 2 for PDF: Use LATEX Library
-# REM Set documentclass to article, report, book, memoir
-#pandoc -o %BUILD%%BOOKNAME%.pdf %CURRENT_DIR%\%TITLE% %CHAPTERS% %TOC% --standalone -t latex -V documentclass=%LATEX_CLASS%
+    $PANDOC_COMMAND_ODT
+done
 
-# Generating open document
-echo "Generating open document"
-PANDOC_COMMAND_ODT="$PANDOC_COMMAND --output=$BUILD$BOOKNAME.odt $CHAPTERS  --to=odt"
-
-$PANDOC_COMMAND_ODT
