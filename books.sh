d1bb290beb71ef72d516f1c3033b7da24a67e0fe
codingepaduli <padulodeveloper@protonmail.com>
2023-05-12 12:48:47 +0200

    Fixed script for using absolute path for each file

--- books.sh
+++ books.sh
@@ -3,10 +3,14 @@
 Elenco di ebook da creare a partire dagli articoli elencati
 
 For each post, set the weight variable in the Hugo Frontmatter following the rules below:
-    HTML: 10000 + 60 * x
-    JS: 10000 + 60 * x + 20
-    P5JS: 10000 % 60 * x + 40
+    HTML: 10000 - 60 * x
+    JS: 9000 - 60 * x + 20
+    P5JS: 9000 - 60 * x + 40
 '
+
+#CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# echo "Current dir for books: $CURRENT_DIR"
+# echo "Content dir for books: $CONTENT_DIR"
 
 BOOKNAME_JAVASCRIPT="Appunti-di-laboratorio-di-informatica-su-javascript"
 CHAPTERS_JAVASCRIPT="
@@ -19,7 +23,6 @@ CHAPTERS_JAVASCRIPT="
         $CONTENT_DIR/content/coding/web/javascript/interazioneHtml.md
         $CONTENT_DIR/content/coding/web/javascript/selettoriCSS.md
         $CONTENT_DIR/content/coding/web/javascript/DOM.md
-        $CURRENT_DIR/title.md
         "
 
 : ' da verificare
@@ -37,7 +40,6 @@ CHAPTERS_HTML="
         $CONTENT_DIR/content/coding/web/html/codificaUnicode.md
         $CONTENT_DIR/content/coding/web/html/form.md
         $CONTENT_DIR/content/coding/web/html/Accessibilita.md
-        $CURRENT_DIR/title.md
         "
 
 : ' TO ADD
@@ -61,7 +63,6 @@ CHAPTERS_SISTEMI="
         $CONTENT_DIR/content/coding/shell/PowerShell/PrimiScript.md
         $CONTENT_DIR/content/coding/shell/PowerShell/CmdLet-Filesystem-Path.md
         $CONTENT_DIR/content/coding/shell/PowerShell/CmdLet-Filesystem-FileCartelle.md
-        $CURRENT_DIR/title.md
         "
 
 BOOKNAME_P5_JS="Appunti-di-laboratorio-di-informatica-su-p5-js"
@@ -71,7 +72,12 @@ CHAPTERS_P5_JS="
         $CONTENT_DIR/content/coding/web/p5js/graphicsPrimitives.md
         $CONTENT_DIR/content/coding/web/p5js/graphicsPrimitives_exe.md
         $CONTENT_DIR/content/coding/web/p5js/colorsAndStyles.md
+        $CONTENT_DIR/content/coding/web/p5js/variables.md
         $CONTENT_DIR/content/coding/web/p5js/soundsAndImages.md
-        $CONTENT_DIR/content/coding/web/p5js/interactivity.md
-        $CURRENT_DIR/title.md
         "
+
+: ' TO ADD
+    $CONTENT_DIR/content/coding/web/p5js/intro_exe.md
+    $CONTENT_DIR/content/coding/web/p5js/graphicsPrimitives_exe.md
+    $CONTENT_DIR/content/coding/web/p5js/interactivity.md
+'
